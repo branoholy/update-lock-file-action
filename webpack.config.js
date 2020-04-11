4ddddfd5b19e5dc8e822e5e67a283efa59f50b3e
Branislav Holý <branoholy@gmail.com>
2020-04-11 11:43:11 +0000

    Use webpack to bundle dependencies

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+/* eslint-disable import/no-commonjs */
+
+const path = require('path');
+
+// Loaders
+const babelLoader = {
+  loader: 'babel-loader',
+  options: {
+    configFile: false,
+    babelrc: false,
+    // Babel preset ordering is reversed (last to first).
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          targets: {
+            node: 'current'
+          },
+          modules: false
+        }
+      ],
+      '@babel/preset-typescript'
+    ]
+  }
+};
+
+// Config
+module.exports = (env) => ({
+  mode: env,
+  target: 'node',
+  entry: path.resolve(__dirname, 'src/index.ts'),
+  output: {
+    filename: 'index.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [babelLoader]
+      }
+    ]
+  }
+});
