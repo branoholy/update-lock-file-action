65dccd1ca4445a2207dedf93a00428cc689efa76
Branislav Holý <branoholy@gmail.com>
2020-10-20 17:02:58 +0000

    DEPS: Webpack 5

--- babel.config.js
+++ babel.config.js
@@ -1,35 +1,22 @@
 /* eslint-disable import/no-commonjs */
 
-// Presets
-const envPreset = [
-  '@babel/preset-env',
-  {
-    targets: {
-      node: 'current'
-    },
-    modules: 'commonjs'
-  }
-];
-
-const typescriptPreset = '@babel/preset-typescript';
-
 // Config
 module.exports = (api) => {
   api.cache.using(() => process.env.NODE_ENV === 'development');
 
-  // Plugins run before presets. Plugin ordering is first to last.
-  const plugins = [
-    // Workaround: The option "modules: 'commonjs'" of @babel/preset-env should
-    // transpile import to require but it seems like it doesn't work so
-    // the plugin is used manually.
-    '@babel/plugin-transform-modules-commonjs'
-  ];
-
-  // Preset ordering is reversed (last to first).
-  const presets = [envPreset, typescriptPreset];
-
   return {
-    plugins,
-    presets
+    // Preset ordering is reversed (last to first).
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          targets: {
+            node: 'current'
+          },
+          modules: 'commonjs'
+        }
+      ],
+      '@babel/preset-typescript'
+    ]
   };
 };
